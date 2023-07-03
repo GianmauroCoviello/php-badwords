12b07828f0e15d8f1f9c2573da50df4342ab92b3
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-03 10:14:05 +0200

    Modifiche e commenti aggiunti. Esercizio completato

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- FORM IN GET COLLEGATO AL SECONDO FILE PHP-->
     <form action="script.php" method="GET">
-        <input type="text" name="parola" placeholder="parola"><br>
-        <textarea name="paragrafo" placeholder="messaggio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</textarea>
+        <!-- INPUT CHE CONTERRA LA PAROLA CENSURATA -->
+        <input type="text" name="badword" placeholder="Key"><br>
+        <!-- INPUT CHE CONTERRA IL PARAGRAFO -->
+        <textarea name="paragrafo" placeholder="messaggio">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi obcaecati officiis commodi provident. Repellat exercitationem nesciunt recusandae facere architecto similique veritatis labore asperiores, quam nostrum nihil ea maxime atque commodi!</textarea><br>
         <button>invia</button>
     </form>
     
--- script.php
+++ script.php
@@ -1,18 +1,40 @@
 <!-- INSERIMENTO VARIABILI IN GET -->
 <?php 
-    $parola= $_GET['parola'];
+    // variabile che conterrà la parola da censurare 
+    $badword= $_GET['badword'];
+    // variabile in GET che conterrà il paragrafo
     $paragrafo= $_GET['paragrafo'];
 
+    // indica la lunghezza del paragrafo
+    $paragraphLenght = strlen($paragrafo);
 
-    // $replacedeString = str_replace('***', 'gnocchi', $nome);
-
-
-    $concat= $parola." ".$paragrafo;
-
-    $replacedeString = str_replace($parola, '***', $concat)." ".strlen($concat);
+    // variabile che sostrituirà tramite la funzione str_replace la parola inserita nel primo input verra sostituita con tre asterischi e con la funzione strlen verra indicata la lunghezza della stringa
+    $replacedeString = str_replace($badword, '***', $paragrafo);
 
 
-    echo $replacedeString;
+    $NewLenght = strlen($replacedeString)
+
   
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Paragraph:</h3>
+    <p><?php echo $paragrafo ?></p>
+    <p><strong>lunghezza:</strong><?php echo $paragraphLenght ?></p>
+    <hr>
+    <h3>Paragraph:</h3>
+    <p><?php echo $replacedeString ?></p>
+    <p><?php echo $NewLenght ?></p>
+
+
+</body>
+</html>
